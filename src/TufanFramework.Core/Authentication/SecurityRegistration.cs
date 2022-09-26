fb1e69469104263ad5b5a57a3d087f518d336494
tufan.ozdemir <tufan.ozdemir@OBL-COM-0285>
2022-09-26 21:52:50 +0300

    signalR message receive security added

--- src/TufanFramework.Core/Authentication/SecurityRegistration.cs
+++ src/TufanFramework.Core/Authentication/SecurityRegistration.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using TufanFramework.Common.Configuration;
@@ -10,7 +12,7 @@ namespace TufanFramework.Core.Authentication
 {
     public static class SecurityRegistration
     {
-        public static IServiceCollection AddSecurity(this IServiceCollection services, IConfigResolver configResolver)
+        public static IServiceCollection AddSecurity(this IServiceCollection services, IConfigResolver configResolver, List<string> messageChannels = null)
         {
             AuthenticationConfig configuration = null;
             configuration = configResolver.Resolve<AuthenticationConfig>();
@@ -62,6 +64,24 @@ namespace TufanFramework.Core.Authentication
                             ctx.Response.Headers.Add("Token-Expired", true.ToString().ToLower());
                         }
                         ctx.Fail("Not Authorized");
+                        return Task.CompletedTask;
+                    },
+                    OnMessageReceived = ctx =>
+                    {
+                        if(messageChannels == null)
+                        {
+                            return Task.CompletedTask;
+                        }
+
+                        var accessToken = ctx.Request.Query["access_token"];
+
+                        // If the request is for our hub...
+                        var path = ctx.HttpContext.Request.Path;
+                        if (!string.IsNullOrEmpty(accessToken) && messageChannels.Any(c=> path.StartsWithSegments(c)))
+                        {
+                            // Read the token out of the query string
+                            ctx.Token = accessToken;
+                        }
                         return Task.CompletedTask;
                     }
                 };
